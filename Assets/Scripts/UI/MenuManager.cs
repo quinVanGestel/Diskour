b00c343fc0f55afa8318272b2b18b6f59cc2a074
Quin <quinvangestel@gmail.com>
2026-09-10 12:35:32 +0200

    made some ui thingies

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+[SerializeField] private string closeGameText;
+[SerializeField] private string toMenuText;
+[SerializeField] private string goBackMenuStepText;
+
+}
